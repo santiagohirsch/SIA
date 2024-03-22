a0417d0f688e099396a4f9b74ba1b3ad96195889
santiagohirsch <shirsch@itba.edu.ar>
2024-03-22 17:23:55 -0300

    added search duration for AStar algorithm

--- tp1/algorithms/AStar.py
+++ tp1/algorithms/AStar.py
@@ -3,11 +3,12 @@ from classes.StateUtils import StateUtils
 from algorithms import AuxNode
 from algorithms.AlgorithmUtils import Heuristics
 import heapq
+import time
 
 class AStar:
-
     @staticmethod
     def search(initial_state):
+        start_time = time.time()
         qty = 0
         frontier = []
         heapq.heappush(frontier, AuxNode(Node(None, initial_state), 0))
@@ -18,8 +19,10 @@ class AStar:
             node = aux.node
 
             if node.state.is_solution():
+                end_time = time.time()  
+                duration = end_time - start_time  
                 StateUtils.print_solution('A*', qty, node)
-                return True
+                return True, duration 
             
             for child in node.get_children():
                 new_cost = total_cost[node] + 1
@@ -29,4 +32,5 @@ class AStar:
                     heapq.heappush(frontier, AuxNode(child, cost))
 
             qty += 1
-        return False
+        duration = time.time() - start_time
+        return False, duration  
