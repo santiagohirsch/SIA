cb9675b8f697ae56cd87242ee9d2a0218678e832
santiagohirsch <shirsch@itba.edu.ar>
2024-03-22 17:36:40 -0300

    added GlobalGreedy implementation
    Co-authored-by: Matias Luchetti <matiluchetti@users.noreply.github.com>

---
+++ tp1/algorithms/GlobalGreedy.py
@@ -0,0 +1,35 @@
+from classes.Node import Node
+from classes.StateUtils import StateUtils
+from algorithms.AlgorithmUtils import AuxNode, Heuristics
+import heapq
+import time
+
+class GlobalGreedy:
+    @staticmethod
+    def search(initial_state):
+        start_time = time.time()
+        qty = 0
+        visited_nodes = set()
+        queue = []
+        root = Node(None, initial_state)
+        heapq.heappush(queue, AuxNode(root, 0))
+        
+        while queue:
+            aux = heapq.heappop(queue)
+            node = aux.node
+            
+            if node.state.is_solution():
+                end_time = time.time()
+                duration = end_time - start_time
+                StateUtils.print_solution('Global Greedy', qty, node)
+                return True, duration
+            
+            if node not in visited_nodes:
+                visited_nodes.add(node)
+                for child in node.get_children():
+                    value = Heuristics.manhattan_distance(child.state)
+                    heapq.heappush(queue, AuxNode(child, value))
+            
+            qty += 1
+        duration = time.time() - start_time
+        return False, duration
