3e5c96b2ca72517b2ab0f4c7bce59cfcc6cbd97c
Matias Luchetti <mluchetti@itba.edu.ar>
2024-03-24 18:49:33 -0300

    Started graph creations

---
+++ tp1/graphCreation.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Leer el archivo CSV
+df = pd.read_csv('results.csv')
+
+# Asegúrate de que 'Algorithm' y 'Duration' están en tus columnas
+assert 'Algorithm' in df.columns, "La columna 'Algorithm' no se encuentra en el CSV"
+assert 'Duration' in df.columns, "La columna 'Duration' no se encuentra en el CSV"
+
+df2 = df[df['Algorithm'] == 'iddfs']
+df = df[df['Algorithm'] != 'iddfs']
+
+
+grouped = df.groupby('Algorithm')['Duration'].mean()
+std_dev = df.groupby('Algorithm')['Duration'].std()
+
+iddfs = df2['Duration'].mean()
+iddfs_std = df2['Duration'].std()
+
+grouped.plot(kind='bar', yerr=std_dev)
+grouped.plot(kind='bar')
+
+plt.xlabel('Algorithm')
+plt.ylabel('Average Duration')
+plt.title('Comparison of Algorithm Durations')
+
+# Mostrar el gráfico
+plt.show()
+
+
+iddfs.plot(kind='bar', yerr=iddfs_std)
+iddfs.plot(kind='bar')
+
+plt.xlabel('IDDFS')
+plt.ylabel('Average Duration')
+plt.title('IDDFS average duration')
+
+plt.show()
---
+++ tp1/graphsComparison.py
@@ -0,0 +1,46 @@
+import sys
+import importlib
+from classes.SokobanUtils import SokobanUtils
+from algorithms.AlgorithmUtils import algorithm_normalizer
+from algorithms.AlgorithmUtils import heuristic_normalizer
+from algorithms.AlgorithmUtils import ALGORITHMS
+from classes.State import State
+from algorithms.AlgorithmUtils import HEURISTICS
+import csv
+
+
+
+if __name__ == "__main__":
+
+    map_path = 'maps/map1.txt'
+    try:
+        with open(map_path, 'r') as file:
+            sokoban_board = file.read()
+    except FileNotFoundError:
+        print(f"Error: Map {map_path} not found.")
+
+    parsed_positions = SokobanUtils.parse_sokoban_board(sokoban_board)
+
+    walls = parsed_positions.get('wall', [])
+    blanks = parsed_positions.get('blank', [])
+    boxes = parsed_positions.get('box', [])
+    player = parsed_positions.get('player', [])[0]
+    goals = parsed_positions.get('goal', [])
+    deadlocks = SokobanUtils.get_deadlocks(walls, blanks)
+    with open('results.csv', 'a', newline='') as file:
+        writer = csv.writer(file)  
+        writer.writerow(['Algorithm', 'Duration', 'Nodes Expanded', 'Frontier Nodes'])
+
+    for algorithm in ALGORITHMS:
+        for _ in range(100):
+                try:
+                    imported_module = importlib.import_module(f"algorithms.{algorithm_normalizer(algorithm)}")
+                except ModuleNotFoundError:
+                    sys.exit(1)
+                algorithm_class = getattr(imported_module, algorithm_normalizer(algorithm))
+                success, duration, nodes, frontier_nodes = algorithm_class.search(State(set(boxes), set(walls), player, set(goals), set(deadlocks)), 'manhattan_distance')
+                with open('results.csv', 'a', newline='') as file:
+                    writer = csv.writer(file)  
+                    writer.writerow([algorithm, duration, nodes, frontier_nodes])
+
+
