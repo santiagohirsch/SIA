ca193f59dc53ba5d89a05c340c62cde6a1c2120a
Santiago Tomas Medin <smedin@itba.edu.ar>
2024-04-05 19:20:05 -0300

    added attack and defend methods

--- tp2/src/classes/characters/Character.py
+++ tp2/src/classes/characters/Character.py
@@ -1,10 +1,11 @@
 from abc import ABC, abstractmethod
+from src.classes.attributes.AttributeSet import AttributeSet
 
 class Character(ABC):
-    def __init__(self, attack_pct: int, defense_pct: int, attribute):
+    def __init__(self, attack_pct: int, defense_pct: int, attributes: AttributeSet):
         self.attack_pct = attack_pct
         self.defense_pct = defense_pct
-        self.attribute = attribute
+        self.attributes = attributes
 
     def get_attribute(self):
         return self.attribute
@@ -12,16 +13,16 @@ class Character(ABC):
     # HACER
     @abstractmethod
     def attack(self):
-        return (self.attribute)
+        return (self.attributes.get_agility().get_p() + self.attributes.get_expertise().get_p()) * self.attributes.get_strength().get_p() * self.attributes.get_height().get_ATM()
 
     # HACER
     @abstractmethod
-    def defend(self, character):
-        pass
+    def defend(self):
+        return (self.attributes.get_endurance().get_p() + self.attributes.get_expertise().get_p()) * self.attributes.get_health().get_p() * self.attributes.get_height().get_DEM()
 
     @abstractmethod
     def __str__(self):
-        pass
+        return self.attributes.__str__()
 
     def fitness(self):
         return self.attack_pct * self.attack() + self.defense_pct * self.defend()
@@ -30,4 +31,4 @@ class Character(ABC):
         return self.fitness < other.fitness
     
     def __eq__(self, other):
-        return self.attribute == other.attribute
+        return self.attributes == other.attributes
