440848fdb8b354bae2a447061094950b6fdeabe7
Mariano Agopian <maagopian@itba.edu.ar>
2024-04-09 18:44:39 -0300

    Fixed typos in boltzmann method

--- tp2/src/selection/Boltzmann.py
+++ tp2/src/selection/Boltzmann.py
@@ -9,16 +9,18 @@ class Boltzmann(Selection):
     k = 0
     generation = 0
 
+    # TODO: Figure out where to instance function
     def set_boltzmann_params(self, tc: int, t0: int, k: int, generation: int):
         self.tc = tc
         self.t0 = t0
         self.k = k
         self.generation = generation
 
+    @classmethod
     def select(self, population: [], individuals: int): # type: ignore
-        temperature = self.TC + (self.T0 + self.TC) * math.exp(-self.K * self.generation)
+        temperature = self.tc + (self.t0 + self.tc) * math.exp(-self.k * self.generation)
         selected = []
-        fitness = [math.exp(individual.get_fitness()/temperature) for individual in population]
+        fitness = [math.exp(individual.fitness()/temperature) for individual in population]
         average_fitness = sum(fitness) / len(fitness)
         values = [f/average_fitness for f in fitness]
         population_copy = population.copy()
