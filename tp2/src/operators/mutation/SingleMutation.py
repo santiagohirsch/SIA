74d80894c12625f7d100c234131e9d08fe413471
Mariano Agopian <maagopian@itba.edu.ar>
2024-04-09 10:43:18 -0300

    started implementing boltzamnn and changed variable in single mutation

--- tp2/src/operators/mutation/SingleMutation.py
+++ tp2/src/operators/mutation/SingleMutation.py
@@ -6,9 +6,9 @@ class SingleMutation(Mutation):
     @staticmethod
     def mutate(parent: AttributeSet, mutation_rate: float):
         parent_arr = parent.to_array()
-        last_index = len(parent_arr) - 1
+        last_index = len(parent_arr) - 1 #TODO: Check variable name to height_index
         if mutation_rate >= random.random():
-            random_index = random.randint(0, len(parent_arr) - 1)
+            random_index = random.randint(0, last_index)
             if random_index == last_index:
                 parent_arr[random_index] = random.uniform(1.3, 2.0)
             else:
--- tp2/src/selection/Boltzmann.py
+++ tp2/src/selection/Boltzmann.py
@@ -1,6 +1,19 @@
-from src.selection.Selection import Selection
 import random
+import math
+
+from src.selection.Selection import Selection
 
 class Boltzmann(Selection):
+    tc = 0
+    t0 = 0
+    k = 0
+    generation = 0
 
-    def select(population, individuals):
+    def set_temperature(self, tc: int, t0: int, k: int, generation: int):
+        self.tc = tc
+        self.t0 = t0
+        self.k = k
+        self.generation = generation
+
+    def select(self, population: [], individuals: int):
+        temperature = self.TC + (self.T0 + self.TC) * math.exp(-self.K * self.generation)
