abf23f3a5168d3a9bf8669aa5b038c720c098c2f
santiagohirsch <shirsch@itba.edu.ar>
2024-04-09 11:06:16 -0300

    finished boltzmann selection

--- tp2/src/selection/Boltzmann.py
+++ tp2/src/selection/Boltzmann.py
@@ -9,11 +9,27 @@ class Boltzmann(Selection):
     k = 0
     generation = 0
 
-    def set_temperature(self, tc: int, t0: int, k: int, generation: int):
+    def set_boltzmann_params(self, tc: int, t0: int, k: int, generation: int):
         self.tc = tc
         self.t0 = t0
         self.k = k
         self.generation = generation
 
-    def select(self, population: [], individuals: int):
+    def select(self, population: [], individuals: int): # type: ignore
         temperature = self.TC + (self.T0 + self.TC) * math.exp(-self.K * self.generation)
+        selected = []
+        fitness = [math.exp(individual.get_fitness()/temperature) for individual in population]
+        average_fitness = sum(fitness) / len(fitness)
+        values = [f/average_fitness for f in fitness]
+        population_copy = population.copy()
+
+        for _ in range(individuals):
+            individual = random.choices(population_copy, values)[0]
+            selected.append(individual)
+            index = population_copy.index(individual)
+            population_copy.pop(index)
+            values.pop(index)
+        return selected
+
+
+            
