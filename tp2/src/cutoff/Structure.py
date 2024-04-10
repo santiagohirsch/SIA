55125cdcb2049c3fd278dac68a809c5f35852a6f
Matias Luchetti <mluchetti@itba.edu.ar>
2024-04-10 17:05:43 -0300

    Fixed Structure algorithm

--- tp2/src/cutoff/Structure.py
+++ tp2/src/cutoff/Structure.py
@@ -7,10 +7,13 @@ class Structure(Cutoff):
 
     @staticmethod
     def should_cutoff(current_population, old_population, generation, cutoff_value):
+        if(generation == 0):
+            return False
         mutual_elements = current_population
         times = len(old_population)
-        for i in range(times - GEN_MAX, times):
-            mutual_elements = list.filter(lambda x: x in mutual_elements, old_population[i])
+        gen_index = max(0, times - GEN_MAX)
+        for i in range(gen_index, times):
+            mutual_elements = [x for x in mutual_elements if x in old_population[i]]
             if len(mutual_elements)/len(current_population) < cutoff_value:
                 return False
         return True
