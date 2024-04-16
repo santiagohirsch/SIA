2681a79688cc6cd8f24e156812bf4675251b5752
santiagohirsch <shirsch@itba.edu.ar>
2024-04-16 14:52:33 -0300

    fixed expertise_p

--- tp2/src/classes/attributes/Expertise.py
+++ tp2/src/classes/attributes/Expertise.py
@@ -7,7 +7,7 @@ class Expertise(Attribute):
         super().__init__(value)
 
     def get_p(self):
-        return 0.06 * tanh(0.01 * self.value)
+        return 0.6 * tanh(0.01 * self.value)
 
     def __str__(self):
         return super().__str__()
