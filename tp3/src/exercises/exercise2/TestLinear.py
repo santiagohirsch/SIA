62e3c5b8aaf3d361fb8933d16940dfa0b9e8729b
santiagohirsch <shirsch@itba.edu.ar>
2024-05-07 18:22:13 -0300

    linear and non linear perceptron tests

--- tp3/src/exercises/exercise2/TestLinear.py
+++ tp3/src/exercises/exercise2/TestLinear.py
@@ -1,32 +1,43 @@
+from math import ceil
 import numpy as np
+import pandas as pd
 from Perceptron import Perceptron
 from GradientDescent import GradientDescent
 import MathFunctions
 
 def TestLinear():
-    training_set = [
-        [1, 1.0],
-        [1, 2.0],
-        [1, 3.0],
-        [1, 4.0],
-        [1, 5.0]
-    ]
-    training_expected = [2.0, 4.0, 6.0, 8.0, 10.0]
-    test_set = [
-        [1, 1.5],
-        [1, 3.0],
-        [1, 6.0]
-    ]
-    test_expected = [
-        3,
-        6,
-        12
-    ]
-    perceptron = Perceptron(2, GradientDescent, 0.01, MathFunctions.LINEAR, MathFunctions.LINEAR_DERIVATIVE)
-    w_min, training_errors = perceptron.train(training_set, training_expected, 4, 30000, 0.01)
-    results = perceptron.test(test_set, w_min)
-
-    print('expected: ', test_expected)
+    df = pd.read_csv("TP3-ej2-conjunto.csv")
+    training_set = df[['x1', 'x2', 'x3']]
+    expected = df['y']
+    
+    perceptron = Perceptron(3,GradientDescent, 0.1, MathFunctions.LINEAR, MathFunctions.LINEAR_DERIVATIVE)
+    training_data, training_expected, testing_data, testing_expected = split_data(training_set.values.tolist(), expected.values.tolist(), 0.8)
+    w_min, errors = perceptron.train(training_data, training_expected, 2, 1000, 2.0)
+    results = perceptron.test(testing_data, w_min)
+    mse = 0
+    for i in range(0, len(results)):
+        mse += (results[i] - testing_expected[i])**2
+    mse = mse / len(results)
+    
+    print('expected: ', testing_expected)
     print('results: ', results)
+    print('mse: ', mse)
+
+def split_data(data_set, expected, percentage):
+    training_data = []
+    training_expected = []
+    testing_data = []
+    testing_expected = []
+    training_qty = ceil(len(data_set) * percentage)
+    while len(training_data) < training_qty:
+        i = np.random.randint(0, len(data_set) - 1)
+        training_data.append(data_set[i])
+        training_expected.append(expected[i])
+        data_set.pop(i)
+        expected.pop(i)
+
+    testing_data = data_set
+    testing_expected = expected
+    return training_data, training_expected, testing_data, testing_expected
 
 TestLinear()
--- tp3/src/exercises/exercise2/test_non_linear.py
+++ tp3/src/exercises/exercise2/test_non_linear.py
@@ -1,3 +1,4 @@
+from math import ceil
 import os as os
 import pandas as pd
 import numpy as np
@@ -10,17 +11,35 @@ import MathFunctions
 def test_non_linear():
 
     df = pd.read_csv("TP3-ej2-conjunto.csv")
-    training_set = df[['x1', 'x2', 'x3']].to_numpy()
-    expected_set = df['y'].to_numpy()
+    training_set = df[['x1', 'x2', 'x3']]
+    expected_set = df['y']
     min_val = min(expected_set)
     max_val = max(expected_set)
-    expected = np.interp(expected_set, (min_val, max_val), (-1, 1)).tolist()
-    perceptron = Perceptron(3,GradientDescent, 0.1,MathFunctions.LINEAR, MathFunctions.LINEAR_DERIVATIVE)
-    w_min, errors = perceptron.train(training_set, expected, 1, 30000, 2.0)
-    results = perceptron.test(training_set, w_min)
+    expected = np.interp(expected_set, (min_val, max_val), (0, 1)).tolist()
+    perceptron = Perceptron(3, GradientDescent, 0.1, MathFunctions.SIGMOID, MathFunctions.SIGMOID_DERIVATIVE)
+    training_data, training_expected, testing_data, testing_expected = split_data(training_set.values.tolist(), expected, 0.8)
+    w_min, errors = perceptron.train(training_data, training_expected, 1, 30000, 0.1)
+    results = perceptron.test(testing_data, w_min)
 
     print('expected: ', expected)
     print('results: ', results)
 
+
+def split_data(data_set, expected, percentage):
+    training_data = []
+    training_expected = []
+    testing_data = []
+    testing_expected = []
+    training_qty = ceil(len(data_set) * percentage)
+    while len(training_data) < training_qty:
+        i = np.random.randint(0, len(data_set) - 1)
+        training_data.append(data_set[i])
+        training_expected.append(expected[i])
+        data_set.pop(i)
+        expected.pop(i)
+
+    testing_data = data_set
+    testing_expected = expected
+    return training_data, training_expected, testing_data, testing_expected
 
 test_non_linear()
