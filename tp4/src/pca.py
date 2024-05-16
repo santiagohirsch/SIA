6f5d71c56df405a3ba85e7d1fc17897bdc320c16
smedin <smedin@itba.edu.ar>
2024-05-16 18:10:30 -0300

    pca boxplot done
    Co-Authored-By: santiagohirsch <72401657+santiagohirsch@users.noreply.github.com>

--- tp4/src/pca.py
+++ tp4/src/pca.py
@@ -23,18 +23,24 @@ def pca():
     scaler = StandardScaler()
     data = scaler.fit_transform(DATA)
 
+    # BIPLOT
     pca = PCA(n_components=7)
     pca_fitted = pca.fit(data)
     fitted_data = pca_fitted.transform(data)
     load_vector = pca.components_
     load_vector = load_vector/np.linalg.norm(load_vector, axis=1)[:, np.newaxis]
-    bi_plot(fitted_data, COUNTRIES, load_vector, HEADER[1:])
+    # bi_plot(fitted_data, COUNTRIES, load_vector, HEADER[1:])
+
+    # BOXPLOT
+    df = pd.DataFrame(DATA).astype(float)
+    df = df.apply(lambda x: (x - np.mean(x)) / np.std(x), axis=0)
+    box_plot(df.values.tolist(), HEADER[1:])
 
     
 
 
 def bi_plot(fitted_data, countries, load_vector, vars):
-    df =  pd.DataFrame(fitted_data, columns=['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7'])
+    df = pd.DataFrame(fitted_data, columns=['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7'])
     df['Country'] = countries
     plt.figure(figsize=(10, 10))
     plt.scatter(df['PC1'], df['PC2'], s=100)
@@ -52,6 +58,20 @@ def bi_plot(fitted_data, countries, load_vector, vars):
     plt.grid()
     plt.show()
 
+def box_plot(data, header):
+    df = pd.DataFrame(data, columns=header)
+    plt.figure(figsize=(10, 10))
+    box = plt.boxplot(df.values, patch_artist=True)
+    
+    colors = ['lightblue', 'lightgreen', 'purple', 'lightpink', 'lightyellow', 'lightgray', 'cyan']  # Lista de colores para asignar a cada variable
+    
+    for patch, color in zip(box['boxes'], colors):
+        patch.set_facecolor(color)
+    
+    plt.xticks(range(1, len(header) + 1), header, rotation=45)
+    plt.title('Boxplot')
+    plt.grid(True, axis='y')
+    plt.show()
 
 
 pca()
