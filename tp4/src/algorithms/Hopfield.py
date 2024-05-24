d624234ac83fd8de83a677d470f37971f6f62278
Matias Luchetti <mluchetti@itba.edu.ar>
2024-05-24 17:55:17 -0300

    Implemented Training

--- tp4/src/algorithms/Hopfield.py
+++ tp4/src/algorithms/Hopfield.py
@@ -21,6 +21,7 @@ class Hopfield:
                 weights[i] += self.patterns[mu][index] * self.patterns[mu][i]
             weights[i] = (1 / n) * weights[i]
         return weights
+    
             
     def get_weights_matrix(self):
         matrix = np.zeros(self.n)
@@ -33,3 +34,32 @@ class Hopfield:
         for neuron in self.neurons:
             states.append(neuron.get_state())
         return states
+    
+
+    def energy(self,weights,states):
+        energy = 0
+        for i in enumerate(len(weights[0])):
+            for j in enumerate(len(weights[0])):
+                if i < j:
+                    energy += weights[i][j] * states[i] * states[j]
+        return -1 * energy
+
+    def train(self, epochs: int):
+        energy_array = []
+        for epoch in range(epochs):
+            for neuron in self.neurons:
+                i = self.neurons.index(neuron)
+                net = 0
+                for j, other_neuron in enumerate(self.neurons):
+                    if i == j:
+                        continue
+                    net += other_neuron.get_state() * neuron.get_weights()[j]
+                if net > 0:
+                    neuron.set_state(1)
+                else: 
+                    if net < 0:
+                        neuron.set_state(-1)
+                    else :
+                        print("Error: state was 0")
+            energy_array.append(self.energy(self.get_weights_matrix(), self.get_states()))
+        return energy_array
