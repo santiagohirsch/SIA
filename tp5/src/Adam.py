28080effa9c98bccce78c219dfe53943f2666353
Santiago Tomas Medin <smedin@itba.edu.ar>
2024-06-23 17:42:48 -0300

    adam fixed
    Co-Authored-By: Mariano Agopian <86855572+marianoagopian@users.noreply.github.com>
    Co-Authored-By: santiagohirsch <72401657+santiagohirsch@users.noreply.github.com>
    Co-Authored-By: Matias Luchetti <95775471+matiluchetti@users.noreply.github.com>

--- tp5/src/Adam.py
+++ tp5/src/Adam.py
@@ -15,10 +15,8 @@ class Adam:
         if self.m is None:
             self.m = np.zeros(np.shape(g))
             self.v = np.zeros(np.shape(g))
-        print("g", g.shape)
-        print("m", (1-self.beta1))
-        self.m = self.beta1 * self.m 
-        self.v = self.beta2 * self.v + (1 - self.beta2) * (g ** 2)
-        m_hat = self.m / (1 - self.beta1 ** epoch + 1)
-        v_hat = self.v / (1 - self.beta2 ** epoch + 1)
-        return self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
+        self.m = self.beta1 * self.m + (1 - self.beta1) * g
+        self.v = self.beta2 * self.v + (1 - self.beta2) * (np.power(g, 2))
+        m_hat = np.divide(self.m, 1 - self.beta1 ** (epoch + 1))
+        v_hat = np.divide(self.v, 1 - self.beta2 ** (epoch + 1))
+        return -self.learning_rate * np.divide(m_hat, (np.sqrt(v_hat) + self.epsilon))
